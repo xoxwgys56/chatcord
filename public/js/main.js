cc18841530cfe164332b58e4a551b45850ee10a5
xoxwgys56 <lololtoday@gmail.com>
2020-03-27 14:00:10 +0900

    Remove and Recreate utils dir

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+// front-end
+
+const chatForm = document.getElementById('chat-form');
+const chatMessage = document.querySelector('.chat-messages');
+
+const socket = io();
+// Message from the server
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  chatMessage.scrollTop = chatMessage.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // Get message text
+  const msg = e.target.elements.msg.value;
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `
+  <p class="meta">
+    Brad <span>9:12pm</span>
+  </p>
+  <p class="text">
+    ${message}
+  </p>`;
+
+  document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+  // send single client
+  socket.emit('message', 'Welcome to ChatCord!');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    // send broadcast
+    io.emit('message', 'A user has left the chat');
+  });
+
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg);
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running  on port ${PORT}`));
